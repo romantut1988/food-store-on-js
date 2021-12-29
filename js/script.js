fea1b6072743a773f226b7e0c7e3eaef18f2ee6f
Roman <romantut1988@mail.ru>
2021-12-30 01:22:25 +0300

    slider images

--- js/script.js
+++ js/script.js
@@ -3,15 +3,35 @@ window.addEventListener('DOMContentLoaded', () => {
           tabsContent = document.querySelectorAll('.tabcontent'),
           tabsParent = document.querySelector('.tabheader__items');
 
-    function hidetabContent() {
+    function hideTabContent() {
         tabsContent.forEach(item => {
-            item.style.display = 'none';
+            item.style.classList.add('hide');
+            item.style.classList.remove('show', 'fade');
         });
 
-        tabs.forEach(tab => {
+        tabs.forEach(item => {
             item.classList.remove('tabheader__item_active');
         });
     }
 
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show',);
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
 
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach( (item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
 });
